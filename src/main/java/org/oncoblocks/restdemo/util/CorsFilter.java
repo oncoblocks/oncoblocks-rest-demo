66221bed0961a2d7509635341aca2cc59fa7d0e4
woemler <woemler@blueprintmedicines.com>
2014-10-20 09:26:52 -0400

    Added CORS filter.

---
+++ src/main/java/org/oncoblocks/restdemo/util/CorsFilter.java
@@ -0,0 +1,28 @@
+package org.oncoblocks.restdemo.util;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by woemler on 10/20/14.
+ */
+public class CorsFilter implements Filter {
+
+	@Override public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+			FilterChain filterChain) throws IOException, ServletException {
+
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+		filterChain.doFilter(servletRequest, servletResponse);
+
+	}
+
+	@Override public void init(FilterConfig filterConfig) throws ServletException { }
+
+	@Override public void destroy() { }
+	
+}
