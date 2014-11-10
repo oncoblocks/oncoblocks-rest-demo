ff23972c35891b69e092b1b4f3758527e2a89492
Ethan Cerami <cerami@gmail.com>
2014-11-10 09:44:07 -0500

    updated to return null objects

--- src/main/java/org/oncoblocks/restdemo/services/EntrezGeneService.java
+++ src/main/java/org/oncoblocks/restdemo/services/EntrezGeneService.java
@@ -1,12 +1,10 @@
 package org.oncoblocks.restdemo.services;
 
-import org.oncoblocks.restdemo.dao.EntrezGeneDao;
+import java.util.List;
+
 import org.oncoblocks.restdemo.models.EntrezGene;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * Created by woemler on 9/26/14.
@@ -16,41 +14,48 @@ import java.util.List;
 @Transactional
 public class EntrezGeneService {
 	
-	@Autowired
-	private EntrezGeneDao entrezGeneDao;
+//	@Autowired
+//	private EntrezGeneDao entrezGeneDao;
 
 	// Find all
 	public List<EntrezGene> findAllEntrezGenes(Integer limit, Integer offset){
-		return entrezGeneDao.findAllEntrezGenes(limit, offset);
+		//		return entrezGeneDao.findAllEntrezGenes(limit, offset);
+		return null;
 	}
 
 	// Find by ID
 	public EntrezGene findEntrezGeneById(Integer id){
-		return entrezGeneDao.findEntrezGeneById(id);
+		//return entrezGeneDao.findEntrezGeneById(id);
+		return null;
 	}
 
 	// Find by attribute
 	public List<EntrezGene> findEntrezGenesByGeneSymbol(String geneSymbol, Integer limit, Integer offset){
-		return entrezGeneDao.findEntrezGenesByGeneSymbol(geneSymbol, limit, offset);
+		//return entrezGeneDao.findEntrezGenesByGeneSymbol(geneSymbol, limit, offset);
+		return null;
 	}
 
 	// Add gene
 	public EntrezGene addEntrezGene(EntrezGene entrezGene){
-		Integer entrezGeneId = entrezGeneDao.addEntrezGene(entrezGene);
-		if (entrezGeneId != null && entrezGeneId > 0){
-			entrezGene = entrezGeneDao.findEntrezGeneById(entrezGeneId);
-		}
-		return entrezGene;
+		//Integer entrezGeneId = entrezGeneDao.addEntrezGene(entrezGene);
+		
+//		if (entrezGeneId != null && entrezGeneId > 0){
+//			entrezGene = entrezGeneDao.findEntrezGeneById(entrezGeneId);
+//		}
+//		return entrezGene;
+		return null;
 	}
 
 	// Update gene
 	public Integer updateEntrezGene(EntrezGene entrezGene){
-		return entrezGeneDao.updateEntrezGene(entrezGene);
+		//return entrezGeneDao.updateEntrezGene(entrezGene);
+		return null;
 	}
 
 	// Delete gene
 	public Integer deleteEntrezGene(Integer id){
-		return entrezGeneDao.deleteEntrezGene(id);
+		//return entrezGeneDao.deleteEntrezGene(id);
+		return null;
 	}
 	
 }
--- src/main/java/org/oncoblocks/restdemo/services/RnaSeqGeneExpressionService.java
+++ src/main/java/org/oncoblocks/restdemo/services/RnaSeqGeneExpressionService.java
@@ -1,12 +1,10 @@
 package org.oncoblocks.restdemo.services;
 
-import org.oncoblocks.restdemo.dao.RnaSeqGeneExpressionDao;
+import java.util.List;
+
 import org.oncoblocks.restdemo.models.RnaSeqGeneExpression;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * Created by woemler on 10/2/14.
@@ -16,27 +14,32 @@ import java.util.List;
 @Transactional
 public class RnaSeqGeneExpressionService {
 	
-	@Autowired
-	private RnaSeqGeneExpressionDao rnaSeqGeneExpressionDao;
+//	@Autowired
+//	private RnaSeqGeneExpressionDao rnaSeqGeneExpressionDao;
 	
 	public RnaSeqGeneExpression findRnaSeqGeneExpressionById(Integer id){
-		return rnaSeqGeneExpressionDao.findRnaSeqGeneExpressionById(id);
+//		return rnaSeqGeneExpressionDao.findRnaSeqGeneExpressionById(id);
+		return null;
 	}
 	
 	public List<RnaSeqGeneExpression> findAllRnaSeqGeneExpression(Integer limit, Integer offset){
-		return rnaSeqGeneExpressionDao.findAllRnaSeqGeneExpression(limit, offset);
+//		return rnaSeqGeneExpressionDao.findAllRnaSeqGeneExpression(limit, offset);
+		return null;
 	}
 	
 	public List<RnaSeqGeneExpression> findRnaSeqGeneExpressionByCellLine(Integer cellLineId, Integer limit, Integer offset){
-		return rnaSeqGeneExpressionDao.findRnaSeqGeneExpressionByCellLine(cellLineId, limit, offset);
+		//return rnaSeqGeneExpressionDao.findRnaSeqGeneExpressionByCellLine(cellLineId, limit, offset);
+		return null;
 	}
 	
 	public List<RnaSeqGeneExpression> findRnaSeqGeneExpressionByGene(Integer entrezGeneId, Integer limit, Integer offset){
-		return rnaSeqGeneExpressionDao.findRnaSeqGeneExpressionByGene(entrezGeneId, limit, offset);
+		//return rnaSeqGeneExpressionDao.findRnaSeqGeneExpressionByGene(entrezGeneId, limit, offset);
+		return null;
 	}
 	
 	public Integer addRnaSeqGeneExpression(RnaSeqGeneExpression rnaSeqGeneExpression){
-		return rnaSeqGeneExpressionDao.addRnaSeqGeneExpression(rnaSeqGeneExpression);
+		//return rnaSeqGeneExpressionDao.addRnaSeqGeneExpression(rnaSeqGeneExpression);
+		return null;
 	}
 	
 }
